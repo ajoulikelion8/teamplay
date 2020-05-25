9ceb9f86cb8fcf8bd03d3f109c16466208f2a207
choikwangil <choikwangil0@naver.com>
2020-05-25 18:07:50 +0900

    url 수정

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -4,9 +4,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp', include('myapp.urls')),
-    path('team1', include('team1.urls')),
-    path('team2', include('team2.urls')),
-    path('team3', include('team3.urls')),
-    path('team4', include('team4.urls')),
+    path('myapp/', include('myapp.urls')),
+    path('team1/', include('team1.urls')),
+    path('team2/', include('team2.urls')),
+    path('team3/', include('team3.urls')),
+    path('team4/', include('team4.urls')),
 ]
